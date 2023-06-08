5e9e5ae2d855c34a27a3a375e320cc70b9429dbf
Dayvson Spacca <spacca.dayvson@gmail.com>
2023-06-07 21:53:58 -0300

    Inicio da migração para componentes da pagina Home

--- components/utils/LottiePlayer.tsx
+++ components/utils/LottiePlayer.tsx
@@ -5,3 +5,8 @@ import { Player } from '@lottiefiles/react-lottie-player';
 export default function LottiePlayer() {
   return <Player autoplay loop src={HomeLottie} style={{ height: '300px', width: '600px' }} />;
 }
+
+interface Lottie {
+  width: string;
+  heigth: string;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,16 @@
 import Link from 'next/link';
-import { BsGithub, BsShieldLockFill } from 'react-icons/bs';
+import { BsShieldLockFill } from 'react-icons/bs';
 import { FaBitcoin } from 'react-icons/fa';
 import { ImSvg } from 'react-icons/im';
-import LottiePlayer from '../../components/home/LottiePlayer';
+import LottiePlayer from '../../components/utils/LottiePlayer';
+import Header from '../../components/home/Header';
 
 export default function Home() {
+  const projetos = [{ nome: '' }];
+
   return (
-    <div className="bg-home-primary overflow-hidden">
-      <header className="bg-slate-800 bg-opacity-50 flex justify-around flex-row w-full fixed items-center z-50">
-        <div className="relative">
-          <h1 className="text-home-text font-bold text-3xl">Landing Pages</h1>
-          <div className="absolute w-96 h-64 bg-gradient-home rotate-45 -left-20 -top-24"></div>
-        </div>
-        <nav className="flex gap-5 text-home-text">
-          <a
-            className="p-5 w-28 text-center border-b-4 rounded border-b-transparent hover:border-b-home-secondary"
-            href="#incio"
-          >
-            Início
-          </a>
-          <a
-            className="p-5 w-28 text-center border-b-4 rounded border-b-transparent hover:border-b-home-secondary"
-            href="#projetos"
-          >
-            Projetos
-          </a>
-          <a
-            className="p-5 w-28 text-center border-b-4 rounded border-b-transparent hover:border-b-home-secondary"
-            href="#sobre"
-          >
-            Sobre
-          </a>
-        </nav>
-        <div className="relative">
-          <Link
-            prefetch
-            target="_blank"
-            href="https://github.com/dayvsonspacca/landing-pages"
-            className="flex gap-2 items-center bg-slate-950 rounded p-3 hover:bg-slate-900"
-          >
-            <BsGithub size={32} color="#fff" />
-            <p className="text-home-text">Github</p>
-          </Link>
-          <div className="absolute w-[40rem] h-64 bg-gradient-home rotate-45 -left-52 -top-16 -z-10"></div>
-        </div>
-      </header>
+    <div className="bg-home-primary overflow-hidden pb-80">
+      <Header />
       <main id="#inicio" className="flex justify-center items-center flex-col text-home-text pt-24">
         <div className="text-center">
           <h1 className="text-3xl mb-4">
